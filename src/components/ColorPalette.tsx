434f24c856d637d88217d3a7a0aa317565ed6c8c
Igor Oganesian <igor.oganesian@gmail.com>
2024-01-14 12:28:25 -0500

    working on color selector

---
+++ src/components/ColorPalette.tsx
@@ -0,0 +1,27 @@
+import { ColorPaletteProps } from "../types";
+import './ColorPalette.css';
+
+const ColorPalette = ({ selectedColor, onSelectColor }: ColorPaletteProps) => {
+    const colors = [
+        '#FF5733', '#33FF57', '#3357FF', '#FFFF33', '#FF33FF',
+        '#33FFFF', '#FF5733', '#C70039', '#900C3F', '#581845',
+        '#FFC300', '#DAF7A6', '#FFC0CB', '#800080', '#008080',
+        '#808000', '#000080', '#DC143C'
+    ];
+
+
+    return (
+        <div className="color-palette">
+            {colors.map(color => (
+                <div
+                    key={color}
+                    className={`color-palette-circle ${selectedColor === color ? 'selected' : ''}`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => onSelectColor(color)}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default ColorPalette;
